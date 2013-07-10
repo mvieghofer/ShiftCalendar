200dcd4b5d35a3cb40027cc48552906bbafc28bd
Markus Vieghofer <markus.vieghofer@gmail.com>
2013-07-10 14:03:57 +0200

    finished first version of shift type management

---
+++ web-app/js/editShiftType.js
@@ -0,0 +1,118 @@
+var type = "addShiftType";
+var oldShiftName = "";
+
+function parseTime(s) {
+    var part = s.match(/(\d+):(\d+)(?: )?(am|pm)?/i),
+        hh = parseInt(part[1], 10),
+        mm = parseInt(part[2], 10),
+        ap = part[3] ? part[3].toUpperCase() : null;
+        
+    if (ap == "AM") {
+        if (hh == 12) {
+            hh = 0;
+        }
+    }    
+    if (ap == "PM") {
+        if (hh != 12) {
+            hh += 12;
+        }
+    }
+    return { "hh": hh, "mm": mm };
+}
+
+function calcDiff(time1, time2) {
+    var date1 = new Date(2014, 1, 1, time1.hh, time1.mm),
+        date2 = new Date(2014, 1, 1, time2.hh, time2.mm);
+    if (date2 < date1) {
+        date2.setDate(date2.getDate() + 1);
+    }
+    var diff = date2 - date1,
+        msec = diff,
+        hh = Math.floor(msec / 1000 / 60 / 60);
+    msec -= hh * 1000 * 60 * 60;
+    var mm = Math.floor(msec / 1000 / 60);
+    msec -= mm * 1000 * 60;
+    var ss = Math.floor(msec / 1000);
+    msec -= ss * 1000;
+    return { "hh": hh, "mm": mm };
+}
+
+function submitShiftType(eventString, fromTime, toTime) {
+    var shiftType = {"name": $("#shift-type-name").val(), 
+                     "from": fromTime,
+                     "to": toTime};
+    if (eventString == "editShiftTypeComplete")
+        $("body").trigger(eventString, [oldShiftName, shiftType]);
+    else
+        $("body").trigger(eventString, shiftType);
+}
+
+function editShiftType(event, shiftType) {
+    type = "editShiftTypeComplete";
+    oldShiftName = shiftType.name;
+    $("#shift-type-name").val(shiftType.name);
+    $("#from-time").val((shiftType.from.hh < 10 ? "0" + shiftType.from.hh : shiftType.from.hh) + ":" + (shiftType.from.hh < 10 ? "0" + shiftType.from.mm : shiftType.from.mm));
+    $("#from-time").trigger("change");
+    $("#to-time").val((shiftType.to.hh < 10 ? "0" + shiftType.to.hh : shiftType.to.hh) + ":" + (shiftType.to.mm < 10 ? "0" + shiftType.to.mm : shiftType.to.mm));
+    $("#to-time").trigger("change");
+}
+
+$(document).ready(function() {
+    var fromTime = {"hh": 0, "mm": 0},
+        toTime = {"hh": 0, "mm": 0},
+        duration = {"hh": 0, "mm": 0};
+        
+    $("body").on("editShiftType", editShiftType);
+    
+    $("#from-time").change(function() {
+        fromTime = parseTime($(this).val());
+        $("#to-time").timepicker("option", "minTime", $(this).val());
+        if ($("#to-time").val().length > 0) {
+            var date1 = new Date(2014, 1, 1, fromTime.hh, fromTime.mm),
+                date2 = new Date(2014, 1, 1, duration.hh, duration.mm);
+            date1.setHours(date1.getHours() + date2.getHours());
+            date1.setMinutes(date1.getMinutes() + date2.getMinutes());
+            var hours = date1.getHours(),
+                minutes = date1.getMinutes();
+            var time = (hours < 10 ? "0" + hours : hours) + ":" + (minutes < 10 ? "0" + minutes : minutes);
+            $("#to-time").val(time);
+        }
+    });
+    
+    $("#to-time").change(function() {
+        toTime = parseTime($(this).val());
+        duration = calcDiff(fromTime, toTime);
+        $("#duration").html((duration.hh < 10 ? "0" + duration.hh : duration.hh) + ":" + (duration.mm < 10 ? "0" + duration.mm : duration.mm));
+    });    
+    
+    $("#btn-add-shift-type-submit").click(function() {
+            var errorText = "";
+            if($("#shift-type-name").val() === "") {
+                errorText = "no name";
+            } 
+            if ($("#from-time").val() === "") {
+                errorText += "<br />no start time";
+            } 
+            if ($("#to-time").val() === "") {
+                errorText += "<br />no end time";
+            }
+            if (errorText === "")
+                submitShiftType(type, fromTime, toTime);
+            else
+                $("#error").html("<div class=\"row\"><div class=\"cell\">" + errorText + "</div></div>");
+        });
+    
+    $("#from-time").timepicker({
+        "timeFormat": "H:i",
+        "scrollDefaultNow": true
+    });
+    
+    $("#to-time").timepicker({
+        "timeFormat": "H:i",
+        "scrollDefaultNow": true,
+        "minTime": "00:00",
+    	"showDuration": true
+    });
+
+    
+});
--- web-app/js/main.js
+++ web-app/js/main.js
@@ -1,15 +1,104 @@
-function setUp() {
-	var shiftTypes = [ {"name": "ShiftType 1"}, {"name": "ShiftType 2"}, {"name": "ShiftType 3"} ]
-	$.each(shiftTypes, function(index, value) {
-		$('#shift-types').append("<li class=\"sub-nav-entry\"><div>" + shiftTypes[index].name + "</div></li>");
-	});
+var shiftTypeEntryTemplate = "<li class=\"sub-nav-entry\" id=\"{0}\"><div>{0}</div><a href=\"#\">-</a></li>";
+var shiftTypes = [ {"name": "ShiftType 1", "from": {"hh": 7, "mm": 0}, "to": {"hh": 19, "mm": 0}}, 
+                   {"name": "ShiftType 2", "from": {"hh": 7, "mm": 0}, "to": {"hh": 19, "mm": 0}}, 
+                   {"name": "ShiftType 3", "from": {"hh": 7, "mm": 0}, "to": {"hh": 19, "mm": 0}} ]
+var mode = "";
+
+function addShiftType(eventString, shiftType) {
+	$("#shift-types").append(shiftTypeEntryTemplate.replace(/\{0\}/g, shiftType.name));
+	shiftTypes.push(shiftType);
 }
 
-$(function() {
+function editShiftType(eventString, oldShiftName, newShiftType) {
+    $.each(shiftTypes, function(index, value) {
+       if (shiftTypes[index].name == oldShiftName) {
+           shiftTypes[index] = newShiftType;
+       } 
+    });
+    if(oldShiftName !== newShiftType.name) {
+        $("#shift-types > .sub-nav-entry > div:contains('" + oldShiftName + "')").html(newShiftType.name);
+    }
+}
+
+function checkNameUniqueness(eventString, name) {
+    var result = true;
+    $.each(shiftTypes, function(index, value) {
+        if (shiftTypes[index].name === name)
+            result = false;
+    });
+    $("body").trigger("checkNameUniqunessResult", result);
+}
+
+function setUp() {
+	$.each(shiftTypes, function(index, value) {
+		$("#shift-types").append(shiftTypeEntryTemplate.replace(/\{0\}/g, shiftTypes[index].name));
+	});
+	
+    $("#main-cont").load("templates/calendar.html");
+	
+	$("body").on("addShiftType", addShiftType);
+	$("body").on("editShiftTypeComplete", editShiftType);
+	$("body").on("checkNameUniquness", checkNameUniqueness);
+}
+
+function getShiftType(name) {
+    var shiftType;
+    $.each(shiftTypes, function(index, value) {
+        if (shiftTypes[index].name == name) {
+            shiftType = shiftTypes[index];
+        }
+    });
+    return shiftType;
+}
+
+$(document).ready(function() {
 	setUp();
 	
-	$('.sub-nav-entry').click(function () {
-		$(this).addClass("sub-nav-entry-active");
-		$(this).siblings().removeClass("sub-nav-entry-active");
+	$(document).on("click", ".sub-nav-entry", function () {
+	    if (!$(this).hasClass("sub-nav-entry-active")) {
+    		$(this).addClass("sub-nav-entry-active");
+    		$(this).siblings().removeClass("sub-nav-entry-active");
+		} else {
+    		$(this).removeClass("sub-nav-entry-active");
+		}
+		if (mode === "edit") {
+            $("body").trigger("editShiftType", getShiftType($("#shift-types > .sub-nav-entry-active > div").html()));
+        }
+	});
+	
+	$("#btn-add-shift-type").click(function() {
+		$("#main-cont").load("templates/editShiftType.html");
+	});
+	
+	$("#btn-edit-shift-type").click(function() {
+	    if (mode !== "edit") {
+	        mode = "edit";
+    	    $(".sub-nav-entry a").show();
+    	    $(".sub-nav-entry a").click(function () {
+    	        var currentAnchor = $(this);
+    	        if (confirm("Are you sure you want to delete this category?")) {
+    	            var indexToDelete = -1;
+    	            $.each(shiftTypes, function (index, value) {
+    	                if (shiftTypes[index].name === $(currentAnchor.siblings()[0]).html()) {
+    	                    indexToDelete = index;
+                            $("#shift-types li").remove(":contains('" + shiftTypes[index].name + "')");
+	                    }
+    	            });
+    	            if (~indexToDelete) {
+    	                shiftTypes.splice(indexToDelete, 1);
+	                }
+    	        }    
+                return false;
+    	    });
+            $("#main-cont").load("templates/editShiftType.html", function() {
+                if ($("#shift-types > .sub-nav-entry-active").length === 1) {
+            	    $("body").trigger("editShiftType", getShiftType($("#shift-types > .sub-nav-entry-active > div").html()));                
+                }
+            });
+        } else {
+            mode = "calendar";
+    	    $(".sub-nav-entry a").hide();
+            $("#main-cont").load("templates/calendar.html");
+        }
 	});
 });
